7cc83548bdf0a9e17778ecbc3245dcac2c3c12ee
Jorge Venegas <jorgepvenegas@gmail.com>
2019-08-11 11:51:58 -0700

    factorial with memoization

--- factorial.js
+++ factorial.js
@@ -1,5 +1,4 @@
-'use strict';
-
+// Basic factorial
 const factorial = n => {
   if(n <= 1) { 
     return n;
@@ -7,6 +6,31 @@ const factorial = n => {
   return n * factorial(n-1);
 }
 
+// Factorial with memoization
+const memoFactorial = () => {
+  let cache = {};
+  
+  const factorial = n => {
+    if(n <= 1) {
+      return n;
+    }
+    else {
+      if(n in cache) {
+        // console.log(`* Cached value for n: ${n} = ${cache[n]}`);
+        return cache[n]
+      }
+      else {
+        cache[n] = n * factorial(n-1);
+        // console.log(`> Store value for n: ${n} = ${cache[n]}`);
+        return cache[n];
+      }
+    }
+  }
+
+  return factorial;
+}
+
 module.exports = {
-  factorial
+  factorial,
+  memoFactorial
 }
